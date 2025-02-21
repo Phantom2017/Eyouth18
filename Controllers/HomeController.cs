95b9e01519707afb7c9c5f82bae3b827cfef9945
Phantom2017 <eng.mohamed888@gmail.com>
2025-02-21 20:24:47 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+using System.Diagnostics;
+using Eyouth1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eyouth1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //Action
+        //1) public
+        //2) Can't be static
+        //3) Can't be overloaded
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ContentResult Hello()
+        {
+            //prepare
+            ContentResult content = new ContentResult();
+
+            //Set
+            content.Content = "Hello MVC";
+            return content;
+        }
+
+        public ViewResult MyView()
+        {
+            ViewResult view = new ViewResult();
+
+            view.ViewName = "view1";
+            return view;
+        }
+
+        public IActionResult myView2(int myId)
+        {
+            if (myId == 1)
+            {
+                return View("view1");
+            }
+            else
+            {
+                //prepare
+                return Content("Hellow MVC");
+            }
+        }
+
+        //Content ContentResult
+        //View    ViewResult
+        //Json JsonResult
+        //File
+        //NotFound
+        //NotAuthorized
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Eyouth1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ViewStudent()
+        {
+            return View();
+        }
+    }
+}
